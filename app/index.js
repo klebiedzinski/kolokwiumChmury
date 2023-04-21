78dbf329e26bb161ad411825fea3101926d07cb4
klebiedzinski <lebiedzinskik02@gmail.com>
2023-04-21 16:44:38 +0000

    sory za node modules

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3030;
+const port = process.env.PORT || 3030;
 
 app.get('/', (req, res) => {
   const date = new Date();
--- app/script.sh
+++ app/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo docker builder prune -f
-# GIT_REPO=$1
-# echo "GIT_REPO=$GIT_REPO"
+PORT=$1
+echo "PORT=$PORT"
 sudo docker network create kolos_network --driver bridge
-# sudo docker build -t kolokwium --build-arg GIT_REPO=$GIT_REPO .
+sudo docker build -t kolokwium --build-arg PORT=$PORT .
 sudo docker run --rm -d --network kolos_network -p 3030:3030 kolokwium
